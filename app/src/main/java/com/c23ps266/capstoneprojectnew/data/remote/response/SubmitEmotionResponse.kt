da77a4c46254fd4ded46462108b9e2bf5681d59b
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-05-27 21:54:33 +0700

    Add Retrofit helper classes for API calls
    Add the groundwork for later use of API calls. Most of the things are placeholders for now.

---
+++ app/src/main/java/com/c23ps266/capstoneprojectnew/data/remote/response/SubmitEmotionResponse.kt
@@ -0,0 +1,12 @@
+package com.c23ps266.capstoneprojectnew.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Placeholder response class
+ * TODO: get the API response detail from CC then implement this class
+ */
+data class SubmitEmotionResponse(
+	@field:SerializedName("link")
+	val link: String? = null
+)
