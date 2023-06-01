c51a1d55e33d66d3e2e775d3ae8844e647c5c42f
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-06-01 13:51:33 +0700

    Add view model and its factory
    Also add UserModel class to represent User that contains name and email (for now at least). It is created mainly for readibility purpose for MainViewModel.getUserData function.

---
+++ app/src/main/java/com/c23ps266/capstoneprojectnew/ui/ViewModelFactory.kt
@@ -0,0 +1,30 @@
+package com.c23ps266.capstoneprojectnew.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.c23ps266.capstoneprojectnew.data.AppRepository
+import com.c23ps266.capstoneprojectnew.data.Injection
+
+class ViewModelFactory private constructor(
+    private val repository: AppRepository,
+) : ViewModelProvider.NewInstanceFactory() {
+    companion object {
+        private var INSTANCE: ViewModelFactory? = null
+
+        fun getInstance(context: Context): ViewModelFactory = INSTANCE ?: synchronized(this) {
+            ViewModelFactory(Injection.provideRepository()).also { INSTANCE = it }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            // @formatter:off
+            modelClass.isAssignableFrom(MainViewModel::class.java)        -> MainViewModel(repository)
+            modelClass.isAssignableFrom(LoginViewModel::class.java)       -> LoginViewModel(repository)
+            else -> throw java.lang.IllegalArgumentException("Unknown ViewModel class: " + modelClass.name )
+            // @formatter:on
+        } as T
+    }
+}
