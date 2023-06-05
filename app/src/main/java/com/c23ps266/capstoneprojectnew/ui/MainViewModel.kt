dd9079267a47ab8434da8d458bc366c028914d71
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2023-06-06 02:46:16 +0700

    Add support to (un)favorite from audio list
    There's a bug where when the favorite image view is clicked, other unrelated favorite image on other entry on the list will flicker. I still can't pinpoint what caused this.

--- app/src/main/java/com/c23ps266/capstoneprojectnew/ui/MainViewModel.kt
+++ app/src/main/java/com/c23ps266/capstoneprojectnew/ui/MainViewModel.kt
@@ -3,8 +3,11 @@ package com.c23ps266.capstoneprojectnew.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
 import com.c23ps266.capstoneprojectnew.data.AppRepository
+import com.c23ps266.capstoneprojectnew.model.AudioModel
 import com.c23ps266.capstoneprojectnew.model.UserModel
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val repository: AppRepository,
@@ -15,6 +18,11 @@ class MainViewModel(
     fun submitEmotion(emotion: String) {
         emotionInput.value = emotion
     }
+
     fun getUserData(): UserModel? = repository.getUser()?.run { UserModel(displayName, email, photoUrl) }
     fun signOut() = repository.signOut()
+
+    fun addToFavorite(audio: AudioModel) = viewModelScope.launch { repository.addAudioToFavorite(audio) }
+    fun isFavorited(title: String) = repository.isAudioFavorite(title)
+    fun removeFromFavorite(title: String) = viewModelScope.launch { repository.removeAudioFromFavorite(title) }
 }
