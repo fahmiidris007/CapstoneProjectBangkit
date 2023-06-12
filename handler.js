63b48f346d6c8fddbd590763b1515561b8487720
Faza <70759116+TheWiseMan25@users.noreply.github.com>
2023-06-12 10:44:27 +0700

    Final commit

--- handler.js
+++ handler.js
@@ -1,10 +1,19 @@
+//Importing required modules.
 const { Storage } = require("@google-cloud/storage");
+const path = require("path");
 
+//Path to the key file.
+const pathKey = path.resolve("./calmind-33a00-f819f31401f2.json");
+
+//Handler function to get Cloud Storage metadata.
 const getLinkHandler = async (request, h) => {
   const bucketName = "soundcalmind";
   let prefix = request.params.prefix;
+
+  //Capitalize the first letter of the prefix.
   prefix = prefix.charAt(0).toUpperCase() + prefix.slice(1);
 
+  //Get the object names from the bucket.
   try {
     const objectNames = await getObjectByNamePrefix(bucketName, prefix);
     return objectNames;
@@ -13,23 +22,40 @@ const getLinkHandler = async (request, h) => {
     return h.response("Error retrieving objects").code(500);
   }
 };
-
+//Function to get object names from the bucket.
 const getObjectByNamePrefix = async (bucketName, prefix) => {
+  //Create a new storage instance.
   const storage = new Storage({
-    keyFilename: "../calmind-33a00-f819f31401f2.json",
+    keyFilename: pathKey,
   });
   const bucket = storage.bucket(bucketName);
 
+  //Set the prefix.
   const options = {
     prefix: prefix,
     delimiter: "/",
   };
 
+  //Get the objects from the bucket.
   const [files] = await bucket.getFiles(options);
 
-  const objectLinks = files.map((file) => file.metadata.mediaLink);
+  const objectInfo = [];
 
-  return objectLinks;
+  //Get the metadata for each object.
+  for (const file of files) {
+    const [metadata] = await file.getMetadata({ custom: true });
+    const customMetadata = metadata.metadata;
+
+    //Get the link, title, and duration from the metadata.
+    const link = file.metadata.mediaLink;
+    const title = file.name;
+    const duration = Number(customMetadata.duration);
+
+    const object = { link, title, duration };
+    objectInfo.push(object);
+  }
+
+  return objectInfo;
 };
 
 module.exports = getLinkHandler;
