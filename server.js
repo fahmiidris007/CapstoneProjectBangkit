f4e8eb359a4b2179133177af9d18f9a4b9e84b4c
Faza <70759116+TheWiseMan25@users.noreply.github.com>
2023-06-12 10:45:58 +0700

    Final commit

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 
+//Function to initialize the server.
 const init = async () => {
+  //Create a new server instance.
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
-    routes: {
-      cors: {
-        origin: ["*"],
-      },
-    }
+    port: 8080,
   });
 
   server.route(routes);
@@ -18,6 +14,7 @@ const init = async () => {
   console.log("Server running on", server.info.uri);
 };
 
+//Process unhandled promise rejections and exit the process.
 process.on("unhandledRejection", (err) => {
   console.error("Unhandled promise rejection:", err);
   process.exit(1);
